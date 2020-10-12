34216c467c02efd5051cefcd8c20d77e08475349
Mark Corpuz <social@markcorpuz.com>
2020-10-11 19:32:12 -0700

    Creating test templates to tackle issues with flex template

---
+++ partials/views/guidebox.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * TEMPLATE: GUIDEBOX
+ */
+
+
+?>
+
+This template is defined on the setup-log plugin itself called guidebox.php but it's supposed to echo the log_date from the themes item-log.php defined on the theme. This results in an error.
+
+<?php
+
+echo setup_be_log_date();
+
+?><hr /><?php
--- partials/views/view-one.php
+++ partials/views/view-one.php
@@ -7,12 +7,11 @@
 
 ?>
 
-this is the actual one template
+This template is defined on the setup-log plugin itself called view-one.php. It's supposed to get the log_date.
 
 <?php
 
-echo '<h1>'.get_field( 'log_label' ).'</h1>';
+//echo '<h1>'.get_field( 'log_label' ).'</h1>';
+echo '<div class="item log date">' . get_field( 'log_date' ) . '</div>';
 
 ?><hr /><?php
-
-echo setup_be_title();
